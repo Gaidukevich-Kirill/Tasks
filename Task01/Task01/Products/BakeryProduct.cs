3f33993a147750f4e0f4351f0e8cff56abd4a1b8
Kirill Gaidukevich <gaidukevich.kirill@gmal.com>
2025-01-09 22:25:52 +0300

    start task 1

---
+++ Task01/Task01/Products/BakeryProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01.Products
+{
+    internal abstract class BakeryProduct
+    {
+        float Prise {  get; set; }
+        int Calories { get; set; }
+        float Margin { get; set; }
+    }
+}
---
+++ Task01/Task01/Products/Bread.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01.Products
+{
+    internal class Bread : BakeryProduct
+    {
+
+    }
+}
